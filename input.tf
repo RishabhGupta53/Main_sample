71895b527cf19d355e724cc47084f1d66154ef14
RishabhGupta53 <95646246+RishabhGupta53@users.noreply.github.com>
2025-04-21 18:13:40 +0530

    Update input.tf

--- input.tf
+++ input.tf
@@ -1,91 +1,92 @@
-    resource_group_name = "rg-IaCAutomation5-CB-USE2-D-PRI" 
-    location = "eastus2"
-    rg_tags = {
-    "CCoE_ID_Number":  "23-0304",
-    "Application_Name":  "IaCAutomation5",
-    "CCoE_Cloud_Planning_Manager":  "Mario Alberto GonzÃ¡lez Vallejo",
-    "CCoE_Architect":  "Balwant Singh",
-    "Business_ProjectManager":  "Leo Rajapakse \u003cleo.rajapakse@grupobimbo.com\u003e",
-    "Application_Owner":  "alberto.gonzalez06@grupobimbo.com",
-    "Application_Technical_Contact":  "balwant.singh@gbsupport.net",
-    "Distribution_List":  "CCOEArchitecture@gbconnect.onmicrosoft.com",
-    "Entity":  "CB",
-    "DR_HA":  "N/A"
+resource "azurerm_application_gateway" "agw" {
+  # ... other configuration like name, location, sku, etc.
+
+  for_each = var.AppGateway
+
+  backend_address_pool {
+    name = "${each.key}_ip_pool"
+    dynamic "backend_addresses" {
+      for_each = contains(keys(each.value), "backend_pools_ip") && each.value.backend_pools_ip != null ? flatten([
+        for pool in values(each.value.backend_pools_ip) : [
+          for ip in pool : { ip_address = ip }
+        ]
+      ]) : []
+
+      content {
+        ip_address = backend_addresses.value.ip_address
+      }
+    }
+  }
+
+  backend_address_pool {
+    name = "${each.key}_fqdn_pool"
+    dynamic "backend_addresses" {
+      for_each = contains(keys(each.value), "backend_pools_fqdn") && each.value.backend_pools_fqdn != null ? flatten([
+        for pool in values(each.value.backend_pools_fqdn) : [
+          for fqdn in pool : { fqdn = fqdn }
+        ]
+      ]) : []
+
+      content {
+        fqdn = backend_addresses.value.fqdn
+      }
+    }
+  }
+
+  backend_address_pool {
+    name = "${each.key}_vm_pool"
+    dynamic "backend_addresses" {
+      for_each = contains(keys(each.value), "backend_pools_vm") && each.value.backend_pools_vm != null ? flatten([
+        for pool in values(each.value.backend_pools_vm) : [
+          for vm in pool : { fqdn = "${vm}.internal.cloudapp.net" }  # Modify FQDN logic if needed
+        ]
+      ]) : []
+
+      content {
+        fqdn = backend_addresses.value.fqdn
+      }
+    }
+  }
+
+  backend_address_pool {
+    name = "${each.key}_appsvc_pool"
+    dynamic "backend_addresses" {
+      for_each = contains(keys(each.value), "backend_pools_appservice") && each.value.backend_pools_appservice != null ? flatten([
+        for pool in values(each.value.backend_pools_appservice) : [
+          for app in pool : { fqdn = data.azurerm_app_service.app_services["${each.key}_${app}"].default_site_hostname }
+        ]
+      ]) : []
+
+      content {
+        fqdn = backend_addresses.value.fqdn
+      }
+    }
+  }
+
+  # Make sure you include the related data blocks if using App Services
+  dynamic "backend_http_settings" {
+    for_each = [each.value]  # You’ll want to conditionally configure this too
+    content {
+      name                                = each.value.backend_settings_name
+      port                                = tonumber(each.value.port)
+      protocol                            = upper(each.value.listener_protocol)
+      pick_host_name_from_backend_address = true
+      request_timeout                     = 30
+    }
+  }
+
+  # more config...
 }
 
-AppGateway_tags = {
-    "CCoE_ID_Number":  "23-0304",
-    "Application_Name":  "IaCAutomation5",
-    "Entity":  "CB",
-    "Organization":  "CORP",
-    "Change_Ticket":  "CHG0052951",
-    "Deploy_Type":  "Automated",
-    "App_Criticality":  "yes",
-    "Resource_Groups":  "rg-IaCAutomation5-CB-USE2-D-PRI",
-    "Launch_Date":  "21/04/2025",
-    "Resource_Description":  "IaC_Automation_300"
-}
 
-AppGateway = {
-    "AppGateway_1":  {
-                         "Resource_Description":  "IaC_Automation_300",
-                         "application_gateway_name":  "cbtauto5agwcd01",
-                         "tier":  "WAF_v2",
-                         "capacity_type":  "Autoscale",
-                         "autoscale_min_instance_count":  "00",
-                         "autoscale_max_instance_count":  "02",
-                         "manual_instance_count":  "01",
-                         "az_region":  "East US 2",
-                         "availability_zone":  "1,2",
-                         "vnet":  "vn-dev-eus2",
-                         "vnet_rg":  "rgp-allDev-Dev-eus2",
-                         "subnet_name":  "snt-IaCAutomation5-CB-USE2-D-PRI-app-gwt",
-                         "frontend_ip":  "public",
-                         "HTTP2":  "Disabled",
-                         "backend_pool_name":  "cbtauto5agwcd01_BEP",
-                         "backend_ip_required":  null,
-                         "backend_ips":  null,
-                         "backend_fqdns_required":  null,
-                         "backend_fqdns":  null,
-                         "backend_vm":  "true",
-                         "backend_vm_name":  "BBUBRDBUSQLCD01",
-                         "backend_vm_rg":  null,
-                         "backend_vm_nic":  "BBUBRDBUSQLCD01_nic",
-                         "backend_app_service":  null,
-                         "app_service_name":  null,
-                         "backend_app_service_rg":  null,
-                         "listener_name":  "cbtauto5agwcd01_Listener",
-                         "listener_protocol":  "Https",
-                         "key_vault_name":  "CBCOEOPKVACD01",
-                         "key_vault_rg":  "rg-CCoEOpsUtilities-cb-use2-d-pri",
-                         "hostname":  "app.appgateway.com",
-                         "rule_name":  "cbtauto5agwcd01_Rule",
-                         "backend_settings_name":  "cbtauto5agwcd01_BES",
-                         "port":  "443",
-                         "private_ip_address":  null,
-                         "firewall_policy_name":  "CBTAUTO5WAFCD",
-                         "backend_pools_ip":  {
-                                                  "backend_pool_ip_001":  [
-                                                                              "172.28.148.188"
-                                                                          ]
-                                              },
-                         "backend_pools_fqdn":  {
-                                                    "backend_pool_fqdn_001":  [
-                                                                                  "test.appgateway.com"
-                                                                              ]
-                                                },
-                         "backend_pools_vm":  {
-                                                  "backend_pool_vm_001":  [
-                                                                              "BBUBRDBUSQLCD01"
-                                                                          ]
-                                              },
-                         "backend_pools_appservice":  {
-                                                          "backend_pool_appservice_001":  [
-                                                                                              "BBUGBCBUAPSCD01"
-                                                                                          ],
-                                                          "backend_pool_appservice_002":  [
-                                                                                              "BBUGBCBUAPSCD02"
-                                                                                          ]
-                                                      }
-                     }
+
+
+data "azurerm_app_service" "app_services" {
+  for_each            = toset(flatten([
+    for gw_key, gw in var.AppGateway : [
+      for app in flatten(values(gw.backend_pools_appservice)) : "${gw_key}_${app}"
+    ]
+  ]))
+  name                = split("_", each.key)[1]
+  resource_group_name = var.AppGateway[split("_", each.key)[0]].backend_app_service_rg
 }
