9f33d569937f13cab3e7e5d621f611fcff63661a
RishabhGupta53 <95646246+RishabhGupta53@users.noreply.github.com>
2025-04-21 17:31:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -54,70 +54,36 @@ variable "AppGateway" {
     listener_name                = string
     HTTP2                        = string
     hostname                     = string
-    key_vault_name               = string
-    key_vault_rg                 = string
-    backend_ips                  = string # Example: ["10.0.1.10", "10.0.1.11"]
-    backend_ip_required          = string
-    backend_fqdns                = string # Example: ["app.example.com", "test.example.com"]
-    backend_fqdns_required       = string
-    backend_vm                   = bool
-    backend_app_service          = bool
-    listener_protocol            = string
-    app_service_name             = string
-    vnet                         = string
-    vnet_rg                      = string
-    subnet_name                  = string
-    backend_settings_name        = string
-    rule_name                    = string
-    frontend_ip                  = string
-    private_ip_address           = string
+    key_vault_name         = string
+    key_vault_rg           = string
+    # backend_ips            = string # Example: ["10.0.1.10", "10.0.1.11"]
+    # backend_ip_required    = string
+    # backend_fqdns          = string # Example: ["app.example.com", "test.example.com"]
+    # backend_fqdns_required = string
+    # backend_vm             = bool
+    # backend_app_service    = bool
+    listener_protocol      = string
+    #app_service_name       = string
+    vnet                   = string
+    vnet_rg                = string
+    subnet_name            = string
+    backend_settings_name  = string
+    rule_name              = string
+    frontend_ip            = string
+    private_ip_address     = string
     #private_subnet         = string
-    backend_vm_name         = string
-    backend_vm_nic          = string
-    backend_vm_rg           = string
-    app_service_rg          = string
-    firewall_policy_name    = string
-    backend_pool_ip         = string
-    backend_pool_fqdn       = string
-    backend_pool_vm         = string
-    backend_pool_appservice = string
+    # backend_vm_name        = string
+    # backend_vm_nic         = string
+    # backend_vm_rg          = string
+    #app_service_rg = string
+    firewall_policy_name   = string
+    backend_pools_ip         = map(list(string))
+    backend_pools_fqdn       = map(list(string))
+    backend_pools_vm         = map(list(string))
+    backend_pools_appservice = map(list(string))
   }))
 }
 
 variable "AppGateway_tags" {
   type = map(string)
-}
-
-variable "backend_address_pools" {
-  type = list(object({
-    name         = string
-    fqdns        = optional(list(string), [])
-    ip_addresses = optional(list(string), [])
-  }))
-
-  description = "A list of objects containing configuration parameters for backend_address_pool blocks to be created in the Application Gateway."
-
-  validation {
-    condition = !contains(
-      [for i in var.backend_address_pools : (length(i.fqdns) > 0 || length(i.ip_addresses) > 0)],
-      false
-    )
-    error_message = "Each backend address pool must have at least one FQDN or IP Address specified."
-  }
-  #   default = [
-  #   {
-  #     name         = "be-pool-1"
-  #     fqdns        = ["app1.websites.com"]
-  #   },
-  #   {
-  #     name         = "be-pool-2"
-  #     ip_addresses = ["10.0.1.1", "10.0.1.2"]
-  #   },
-  #   {
-  #     name         = "be-pool-3"
-  #     fqdns        = ["app3.websites.com"]
-  #     ip_addresses = ["10.0.3.3"]
-  #   }
-  # ]
-
 }
