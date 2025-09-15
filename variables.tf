54d94ae5e0ed2ba5c5edc9f8d8e4b765874c74a4
RishabhGupta53 <95646246+RishabhGupta53@users.noreply.github.com>
2025-09-15 18:38:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,89 +1,103 @@
+variable "location" {
+  type        = string
+  description = "Azure region where resources will be created"
+  
+  validation {
+    condition     = length(var.location) > 0
+    error_message = "Location cannot be empty."
+  }
+}
 
 variable "resource_group_name" {
   type        = string
-  description = "resource group name"
+  description = "Name of the Resource Group"
+  
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9._()-]{1,90}$", var.resource_group_name))
+    error_message = "Resource group name must be 1-90 characters and can contain alphanumeric, underscore, parentheses, hyphen, and period characters."
+  }
 }
 
-variable "location" {
-  type        = string
-  description = "azure region"
-}
-
-variable "allocation_method" {
-  description = "Public IP allocation method"
-  type        = string
-  default     = "Static"
-}
-
-variable "rule_type" {
-  description = "Type of request routing rule"
-  type        = string
-  default     = "Basic"
-}
-
-variable "sku" {
-  description = "SKU for the public IP (Standard or Basic)"
-  type        = string
-  default     = "Standard"
-}
-
-variable "cert_name" {
-  description = "Self signed certificate name"
-  type        = string
-  default     = "BimboConnect-COM"
-}
-
-variable "managed_identity" {
-  description = "Managed Identity name"
-  type        = string
-  default     = "CBCOEOPMIDCD02"
-}
-
-variable "AppGateway" {
+variable "StorageAccounts" {
   type = map(object({
-    application_gateway_name     = string
-    port                         = string
-    az_region                    = string
-    tier                         = string
-    autoscale_min_instance_count = string
-    autoscale_max_instance_count = string
-    capacity_type                = string
-    manual_instance_count        = string
-    availability_zone            = string
-    backend_pool_name            = string
-    listener_name                = string
-    HTTP2                        = string
-    hostname                     = string
-    key_vault_name         = string
-    key_vault_rg           = string
-    # backend_ips            = string # Example: ["10.0.1.10", "10.0.1.11"]
-    # backend_ip_required    = string
-    # backend_fqdns          = string # Example: ["app.example.com", "test.example.com"]
-    # backend_fqdns_required = string
-    # backend_vm             = bool
-    # backend_app_service    = bool
-    listener_protocol      = string
-    #app_service_name       = string
-    vnet                   = string
-    vnet_rg                = string
-    subnet_name            = string
-    backend_settings_name  = string
-    rule_name              = string
-    frontend_ip            = string
-    private_ip_address     = string
-    #private_subnet         = string
-    # backend_vm_name        = string
-    # backend_vm_nic         = string
-    # backend_vm_rg          = string
-    #app_service_rg = string
-    firewall_policy_name   = string
-    backend_pools_ip         = map(list(string))
-    backend_pools_fqdn       = map(list(string))
-    backend_pools_vm         = map(list(string))
-    backend_pools_appservice = map(list(string))
+    name                    = string
+    account_tier           = string
+    account_replication_type = string
+    kind                   = string
+    tls                    = string
+    copy_scope            = string
+    hns                   = bool
+    sftp                  = bool
+    nfs                   = bool
+    access_tier           = string
+    routing               = string
+    infra_encrypt         = bool
+    feed                  = bool
+    versioning            = bool
+    pep_subnet            = string
+    pep_vnet              = string
+    vnet_rg               = string
+    static_website_enabled = string
   }))
+  description = "Map of storage accounts to create"
+  
+  validation {
+    condition = alltrue([
+      for k, v in var.StorageAccounts : 
+      can(regex("^[a-z0-9]{3,24}$", v.name))
+    ])
+    error_message = "Storage account names must be 3-24 characters long and contain only lowercase letters and numbers."
+  }
+  
+  validation {
+    condition = alltrue([
+      for k, v in var.StorageAccounts : 
+      contains(["Standard", "Premium"], v.account_tier)
+    ])
+    error_message = "Account tier must be either 'Standard' or 'Premium'."
+  }
 }
 
-variable "AppGateway_tags" {
-  type = map(string)
+variable "sto_tags" {
+  type        = map(string)
+  description = "Tags to apply to storage account resources"
+  default     = {}
+}
+
+variable "enable_https" {
+  type        = bool
+  description = "Enable HTTPS traffic only"
+  default     = true
+}
+
+variable "public_access" {
+  type        = bool
+  description = "Enable public network access"
+  default     = false
+}
+
+variable "sas" {
+  type        = bool
+  description = "Enable shared access key"
+  default     = true
+}
+
+variable "is_manual_connection" {
+  type        = bool
+  description = "Is the private endpoint connection manual"
+  default     = false
+}
+
+variable "subresource_names" {
+  type        = list(string)
+  description = "List of subresource names for private endpoint"
+  default     = ["blob"]
+  
+  validation {
+    condition = alltrue([
+      for name in var.subresource_names : 
+      contains(["blob", "file", "web", "dfs"], name)
+    ])
+    error_message = "Subresource names must be one of: blob, file, web, dfs."
+  }
 }
